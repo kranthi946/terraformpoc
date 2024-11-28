9194f24e6ac45e5e24feb64bc51928c171e67e25
kranthi946 <kranthikumarkattatech@gmail.com>
2024-11-28 10:49:46 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("credentials.json")}"
-project = "carbide-trees-336004"
+#credentials = "${file("credentials.json")}"
+project = "ma-cicd"
 region = "us-west1"
 }
