120a1099e3389a5771f470892169059e59c8a2ad
kranthi946 <kranthikumarkattatech@gmail.com>
2024-11-28 15:48:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,9 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  // Inject SSH keys into the instance metadata
-  metadata = {
-   ssh-keys = "rocky:${file("/home/kranthikumarkattatech/.ssh/my_new_vm_key.pub")}"
-  }
+ metadata = {
+  ssh-keys = "rocky:${file("/home/kranthikumarkattatech/.ssh/my_new_vm_key.pub")}"
+}
 
   // Startup script
   metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
