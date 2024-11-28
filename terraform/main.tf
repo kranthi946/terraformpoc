a5adb9ce2d6d0a8a074b081dc7976903ef538a7e
kranthi946 <kranthikumarkattatech@gmail.com>
2024-11-28 16:13:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,29 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
-  
-  // Apply the firewall rule to allow external IPs to access this instance
-  tags = ["http-server"]
+  // Add SSH key to VM metadata
+  metadata = {
+    ssh-keys = "your-username:${file("${path.module}/my_new_vm_key.pub")}"
+  }
 
-  ssh-keys = "rocky:${file("ssh_keys/my_new_vm_key.pub")}"  // Relative path from workspace
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+  tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http-terraform"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080"]
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
