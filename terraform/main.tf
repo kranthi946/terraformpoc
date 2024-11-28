14bb93975165c6913b754106b82fc06ff6b5a826
kranthi946 <kranthikumarkattatech@gmail.com>
2024-11-28 15:55:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,17 +13,14 @@ resource "google_compute_instance" "default" {
     network = "default"
 
     access_config {
-      // This section assigns an external IP address to the instance
+      // Include this section to give the VM an external IP address
     }
   }
 
-metadata = {
-  ssh-keys = "rocky:${file("/home/kranthikumarkattatech/jenkins_ssh_keys/my_new_vm_key.pub")}"
-}
-
-  // Startup script
   metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
-
+  
   // Apply the firewall rule to allow external IPs to access this instance
   tags = ["http-server"]
+
+  ssh-keys = "rocky:${file("ssh_keys/my_new_vm_key.pub")}"  // Relative path from workspace
 }
