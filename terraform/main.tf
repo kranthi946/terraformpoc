2f575e338b91ae04bc85da275e6a00d6513d955f
kranthi946 <kranthikumarkattatech@gmail.com>
2024-11-28 16:17:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,13 +13,12 @@ resource "google_compute_instance" "default" {
     network = "default"
 
     access_config {
-      // Include this section to give the VM an external IP address
+      // Include this section to give the VM an external ip address
     }
   }
 
-  // Add SSH key to VM metadata
   metadata = {
-    ssh-keys = "your-username:${file("${path.module}/my_new_vm_key.pub")}"
+    ssh-keys = "kranthikumarkattatech:${file("${path.module}/ssh_keys/my_new_vm_key.pub")}"
   }
 
   metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
@@ -35,11 +34,10 @@ resource "google_compute_firewall" "http-server" {
     ports    = ["80", "8080"]
   }
 
-  // Allow traffic from everywhere to instances with an http-server tag
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server"]
 }
 
 output "ip" {
-  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
