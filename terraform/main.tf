d072da897e362833efeae256cf11562c3e60a17e
kranthi946 <kranthikumarkattatech@gmail.com>
2024-11-28 11:26:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,5 @@
-provider "google" {
-  project = "ma-cicd"
-  region  = "us-west1"
-}
-
-# Generate a random ID suffix to ensure unique VM names
-resource "random_id" "vm_suffix" {
-  byte_length = 4
-}
-
-# Google Compute Instance with dynamic name
 resource "google_compute_instance" "default" {
-  name         = "terraform-vm-${random_id.vm_suffix.hex}"  # Unique name using random suffix
+  name         = "terraform-vm"
   machine_type = "e2-small"
   zone         = "us-west1-a"
 
@@ -24,33 +13,25 @@ resource "google_compute_instance" "default" {
     network = "default"
 
     access_config {
-      // Allocate a public IP for the instance
+      // Include this section to give the VM an external ip address
     }
   }
 
-  metadata_startup_script = <<-EOT
-    sudo apt-get update && sudo apt-get install apache2 -y
-    echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' \
-    | sudo tee /var/www/html/index.html
-  EOT
-
-  tags = ["http-server"]
+    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+    // Apply the firewall rule to allow external IPs to access this instance
+    tags = ["http-server"]
 }
-
-# Firewall rule to allow HTTP traffic
 resource "google_compute_firewall" "http-server" {
   name    = "default-allow-http-terraform"
   network = "default"
   allow {
     protocol = "tcp"
-    ports    = ["80", "8080"]
+    ports    = ["80","8080"]
   }
-
+  // Allow traffic from everywhere to instances with an http-server tag
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server"]
 }
-
-# Output the external IP address of the VM
 output "ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
