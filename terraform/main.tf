5cc686838e36f4735692bd5ce1fcf10e9ca51a33
kranthi946 <kranthikumarkattatech@gmail.com>
2024-11-29 22:29:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,35 +11,27 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
+
     access_config {
-      // Include this section to give the VM an external IP address
+      // Include this section to give the VM an external ip address
     }
   }
 
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    sudo apt-get update && sudo apt-get install apache2 -y
-    echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html
-
-    # Create a new user and set the password
-    sudo useradd -m -s /bin/bash myuser
-    echo 'myuser:kkkranthi' | sudo chpasswd
-    EOT
-
-  tags = ["http-server"]
+    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+    // Apply the firewall rule to allow external IPs to access this instance
+    tags = ["http-server"]
 }
-
 resource "google_compute_firewall" "http-server" {
   name    = "default-allow-http-terraform"
   network = "default"
   allow {
     protocol = "tcp"
-    ports    = ["80", "8080"]
+    ports    = ["80","8080"]
   }
+  // Allow traffic from everywhere to instances with an http-server tag
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server"]
 }
-
 output "ip" {
-  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
